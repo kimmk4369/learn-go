74674b9ac60c360660aa2e8b1d4aafb868b8dccd
mkkim <killer5634@naver.com>
2021-01-29 15:34:24 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
-	"./accounts"
+	"fmt"
+
+	//"github.com/kimmk4369/learn-go/accounts"
+	"github.com/kimmk4369/learn-go/mydict"
 )
 
-//  시작점
 func main() {
-	account := accounts.NewAccount("kmk")
+	//account := accounts.NewAccount("mk")
+	//account.Deposit(10)
+	//fmt.Println(account)
+
+	dictionary := mydict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(word)
+	}
 }
